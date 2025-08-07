5b69851869085b46e278655d2f2ef9e2df809a64
Alice <aosullivan@netpremacy.com>
2025-08-07 14:51:44 +0100

    fixed syntaxerror issue

--- src/index.ts
+++ src/index.ts
@@ -45,16 +45,16 @@ startServer();
 // If you want to run it as a standalone script, you can comment out the
 // `githubServer` startup code above and uncomment the code below.
 //
-// import { createInterface } from 'node:readline/promises';
-//
-// async function run() {
-//     const chat = ai.chat(chatConfig);
-//      const readline = createInterface(process.stdin, process.stdout);
-//      
-//     while (true) {
-//     const userInput = await readline("You: ");
-//     const response = await chat.send(userInput);
-//     console.log(response.text);
-//     }
-// }
-// run();
+import { createInterface } from 'node:readline/promises';
+
+async function run() {
+    const chat = ai.chat(chatConfig);
+     const readline = createInterface(process.stdin, process.stdout);
+     
+    while (true) {
+    const userInput = await readline.question("You: ");
+    const response = await chat.send(userInput);
+    console.log(response.text);
+    }
+}
+run();
